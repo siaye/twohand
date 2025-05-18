8db09666272fded3b42c3fe311c0bff943e38244
siaye <sia_ye@126.com>
2025-05-18 23:18:36 +0800

    登录注册

--- src/main/java/com/secondhand/SecondHandApplication.java
+++ src/main/java/com/secondhand/SecondHandApplication.java
@@ -1,13 +1,13 @@
-package com.example.secondhand;
+package com.secondhand;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.mybatis.spring.annotation.MapperScan;
 
 @SpringBootApplication
+@MapperScan("com.secondhand.repository")
 public class SecondHandApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(SecondHandApplication.class, args);
     }
-
-}
+} 
